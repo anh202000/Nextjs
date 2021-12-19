2cef92718934b253d41f3d69fc39244a0c17bfb3
Le Phan tuan Anh <anhroyal110@gmail.com>
2021-12-19 21:15:30 +0700

    Auth login use spotify

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,14 +1,36 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Login.module.css'
+import { getProviders, signIn } from 'next-auth/react'
 
-const Login: NextPage = () => {
-  return (
-    <div className=''>
-     
-    </div>
-  )
+export async function getServerSideProps() {
+	const providers = await getProviders()
+
+	return {
+		props: {
+			providers,
+		},
+	}
+}
+
+const Login: NextPage = ({ providers }: any) => {
+	console.log(Object?.values(providers), 'providers.values')
+
+	const a = Object?.values(providers).map((item: any) => item.name)
+	const b: any = Object?.values(providers).map((item: any) => item.id)
+
+	return (
+		<div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
+			<img className="w-52 mb-5" src="https://links.papareact.com/9xl" alt="" />
+
+			<div>
+				<button
+					className="bg-[#18D860] text-white p-5 rounded-full"
+					onClick={() => signIn(b, { callbackUrl: '/' })}
+				>
+					Login with {a}
+				</button>
+			</div>
+		</div>
+	)
 }
 
 export default Login
